e5092be0d58835f002db78bf241db9357bb6ba21
Kyle Gay <rkylegay@gmail.com>
2015-03-21 22:53:44 -0500

    added toggle & blink functions

--- led.js
+++ led.js
@@ -15,10 +15,14 @@ LED.prototype.init = function(config) {
   .name('LED')
   .type('ardled')
   .state('off')
-  .when('off', { allow: ['turnOn']})
-  .when('on', { allow: ['turnOff'] })
+  .when('off', { allow: ['turnOn', 'toggle']})
+  .when('on', { allow: ['turnOff', 'toggle'] })
+  .when('toggle', {allow: ['*']})
+  .when('blink', {allow: ['*']})
   .map('turnOn', this.turnOn)
-  .map('turnOff', this.turnOff);
+  .map('turnOff', this.turnOff)
+  .map('toggle', this.toggle)
+  .map('blink', this.blink);
   this.arduino = new five.Board();
   this.arduino.on('ready', function(){
     self.led = new five.Led(self.pin);
@@ -37,5 +41,27 @@ LED.prototype.turnOff = function(cb){
     var self = this;
     this.led.off();
     this.state = 'off';
+    cb();
+};
+
+LED.prototype.toggle = function(cb){
+    var self = this;
+    if(this.state === 'off'){    
+        this.led.toggle();
+        this.state = 'off';
+    }
+    else if(this.state === 'on'){
+        this.led.toggle();
+        this.state = 'on';
+    }
+    else
+        this.led.stop().off();
+    cb();
+};
+
+LED.prototype.blink = function(cb){
+    var self = this;
+    this.led.blink();
+    this.state = 'blink';
     cb();
 };
