9762fb4e3266e78d28ded3717cfe0e3fd370c71e
Kyle Gay <rkylegay@gmail.com>
2015-04-02 21:15:14 -0500

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var Led = require('./lib/index');
 var app = require('./apps/starter');
 
 //could retrieve from database
-var pinObjects = {{"name": "greenLed", "pin": 13}, {"name": "redLed", "pin":  12}};
+var pinObjects = [{"name": "greenLed", "pin": 13}, {"name": "redLed", "pin":  12}];
 
 zetta()
   .use(Led, pinObjects)
