fb93d4581dca8b483c01ba2d34be0f27e59461fa
Kyle Gay <rkylegay@gmail.com>
2015-04-02 21:08:21 -0500

    allow user to define pin number and name and pass to LED object

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var zetta = require('zetta');
 var Led = require('./lib/index');
 var app = require('./apps/starter');
 
+//could retrieve from database
+var pinObjects = {{"name": "greenLed", "pin": 13}, {"name": "redLed", "pin":  12}};
+
 zetta()
-  .use(Led, 13,12)
+  .use(Led, pinObjects)
 //  .use(app)
   .listen(1337);
 
